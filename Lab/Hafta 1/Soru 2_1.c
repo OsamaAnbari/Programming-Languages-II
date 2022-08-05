a0f2af968a00b559871f2c20b4cc5352ede52326
MHD OSAMA ANBARI <109667942+OsamaAnbari@users.noreply.github.com>
2022-08-05 11:43:42 +0300

    Add files via upload

---
+++ Lab/Hafta 1/Soru 2_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void fonk(void);
+
+int gid = 287;
+
+int main(void){
+	printf("Soru 2_2.c gid degisken degeri: %d/n", gid);
+	
+	fonk();
+	
+	return 0;
+}
---
+++ Lab/Hafta 1/Soru 2_2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+extern int gid;
+
+void fonk(void){
+	printf("deneme 2_2.c gid degisken degeri: %d", gid);
+}
