4dc1b9dd2a66e3d3cd525d0b451317a7131894e8
MHD OSAMA ANBARI <109667942+OsamaAnbari@users.noreply.github.com>
2022-08-05 12:50:42 +0300

    Update Soru 1.c

--- Lab/Hafta 2/Soru 1.c
+++ Lab/Hafta 2/Soru 1.c
@@ -1,3 +1,16 @@
+/*
+1. Klavyeden öğrenci sayısı (en fazla 100) ve ders geçme notunu alarak bu bilgileri tanımlanacak “hesapla” fonksiyonuna 
+gönderen bir “main” fonksiyonu hazırlayınız. “hesapla” fonksiyonu kendisine gelen öğrenci sayısı kadar notu klavyeden 
+okuyarak 100 elemanlı bir diziye kaydetmeli ve geçme notuna göre kaç tane öğrencinin dersten kaldığını bularak 
+fonksiyondan geri döndürürken, sınıf ortalamasını da ekrana yazdırmalıdır. Buna göre aşağıda verilen alanlara “main” ve 
+“hesapla” fonksiyonlarını yazınız.
+Write a "main" function that takes the number of students (less than or equal to 100) and the passing grade from the 
+keyboard and sends this information to the "calculate" function. The "calculate" function will read grades of each 
+student from the keyboard and record them in an array with 100 elements. According to the passing grade, find out 
+how many students failed and return it from function, and print the class average on the function. Accordingly, write 
+"main" and "calculate" functions in the following fields.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
