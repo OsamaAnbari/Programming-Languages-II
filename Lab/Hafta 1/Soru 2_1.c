b8b0a1f2dbae603f11fa2b2f1d748faff9f4de0d
MHD OSAMA ANBARI <109667942+OsamaAnbari@users.noreply.github.com>
2022-08-05 12:11:13 +0300

    Update Soru 2_1.c

--- Lab/Hafta 1/Soru 2_1.c
+++ Lab/Hafta 1/Soru 2_1.c
@@ -1,3 +1,5 @@
+// S2. Aşağıdaki kod parçalarını yazarak ekran çıktılarını yorumlayınız
+
 #include <stdio.h>
 
 void fonk(void);
@@ -5,9 +7,16 @@ void fonk(void);
 int gid = 287;
 
 int main(void){
-	printf("Soru 2_2.c gid degisken degeri: %d/n", gid);
+	printf("Soru 2_1.c gid degisken degeri: %d/n", gid);
 	
 	fonk();
 	
 	return 0;
 }
+
+/*
+Outputs:
+
+Soru 2_1.c gid degisken degeri: 287
+Soru 2_2.c gid degisken degeri: 287
+*/
